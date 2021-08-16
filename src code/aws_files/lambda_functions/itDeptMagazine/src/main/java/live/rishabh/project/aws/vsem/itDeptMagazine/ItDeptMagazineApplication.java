6228bc4ebbfb620748d334564b55405d47a68c1a
rishabh-live <rishabh12536@gmail.com>
2021-08-16 08:17:17 +0530

    Lambda Functions Forlder Init

---
+++ src code/aws_files/lambda_functions/itDeptMagazine/src/main/java/live/rishabh/project/aws/vsem/itDeptMagazine/ItDeptMagazineApplication.java
@@ -0,0 +1,13 @@
+package live.rishabh.project.aws.vsem.itDeptMagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItDeptMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItDeptMagazineApplication.class, args);
+	}
+
+}
---
+++ src code/aws_files/lambda_functions/itDeptMagazine/src/test/java/live/rishabh/project/aws/vsem/itDeptMagazine/ItDeptMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package live.rishabh.project.aws.vsem.itDeptMagazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItDeptMagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
